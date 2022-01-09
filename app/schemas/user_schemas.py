cb7a0a798c2e8b0ba080adc4840405e9bbcdb039
Abiodun Akinola <akinolaemmanuel49@gmail.com>
2022-01-09 16:50:00 +0000

    added user schema

---
+++ app/schemas/user_schemas.py
@@ -0,0 +1,35 @@
+import datetime
+from typing import List, Optional
+
+from pydantic import BaseModel
+
+
+class UserBase(BaseModel):
+    username: str
+
+
+class UserCreate(UserBase):
+    email: str
+    password: str
+
+
+class User(BaseModel):
+    id: int
+    username: str
+    email: str
+    active: bool
+    created: datetime.datetime
+    last_modified: datetime.datetime
+
+    class Config:
+        orm_mode = True
+
+
+class TokenData(BaseModel):
+    access_token: str
+    refresh_token: str
+
+
+class Credentials(BaseModel):
+    username: str
+    password: str
