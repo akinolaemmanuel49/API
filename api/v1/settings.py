8bf9fca1d3d152739e92fe0bbd84cee33b65d1f3
Abiodun Akinola <akinolaemmanuel49@gmail.com>
2022-04-03 20:26:35 +0100

    modified BASE_DIR location

--- api/v1/settings.py
+++ api/v1/settings.py
@@ -3,7 +3,7 @@ import os
 from dotenv import load_dotenv
 
 BASE_DIR = os.path.join(os.path.dirname(
-    os.path.dirname(os.path.abspath(__file__))), '.env')
+    os.path.dirname(os.path.dirname(os.path.abspath(__file__)))), '.env')
 load_dotenv(BASE_DIR)
 
 
@@ -11,12 +11,12 @@ class Settings:
     """
     Base Settings for the project.
     """
-    SECRET_KEY = os.getenv('SECRET_KEY')
+    SECRET_KEY = os.environ.get('SECRET_KEY')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SQLALCHEMY_DATABASE_URI = os.getenv(
-        'DATABASE_URL').replace("postgres", "postgresql")
+    SQLALCHEMY_DATABASE_URI = os.environ.get(
+        'DATABASE_URL').replace("postgres", "postgresql", 1)
     ENCRYPTION_SCHEMES = ['bcrypt']
-    JWT_SECRET_KEY = os.getenv('JWT_SECRET_KEY')
-    JWT_ALGORITHM = os.getenv('JWT_ALGORITHM')
-    JWT_EXPIRATION_MINUTES = int(os.getenv('JWT_EXPIRATION_MINUTES'))
-    JWT_REFRESH_EXPIRE_HOURS = int(os.getenv('JWT_REFRESH_EXPIRE_HOURS'))
+    JWT_SECRET_KEY = os.environ.get('JWT_SECRET_KEY')
+    JWT_ALGORITHM = os.environ.get('JWT_ALGORITHM')
+    JWT_EXPIRATION_MINUTES = int(os.environ.get('JWT_EXPIRATION_MINUTES'))
+    JWT_REFRESH_EXPIRE_HOURS = int(os.environ.get('JWT_REFRESH_EXPIRE_HOURS'))
