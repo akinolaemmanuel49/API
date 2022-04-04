2fa45cc72a91e7352834130d1fcfdfd5fc67e693
Abiodun Akinola <akinolaemmanuel49@gmail.com>
2022-04-04 22:22:10 +0100

    made some user columns not nullable

--- api/v1/db/models/user_model.py
+++ api/v1/db/models/user_model.py
@@ -13,15 +13,30 @@ class User(Base):
     __tablename__ = "users"
 
     id = Column(Integer, primary_key=True, index=True)
-    username = Column(String, unique=True, index=True)
-    email = Column(String, unique=True, index=True)
-    hashed_password = Column(String)
-    active = Column(Boolean, default=True)
-    is_admin = Column(Boolean, default=False)
+    username = Column(String, unique=True, index=True, nullable=False)
+    email = Column(String, unique=True, index=True, nullable=False)
+    hashed_password = Column(String, nullable=False)
+    active = Column(Boolean, index=True, default=True, nullable=False)
+    is_admin = Column(Boolean, index=True, default=False, nullable=False)
 
     posts = relationship("Post", back_populates="owner")
     comments = relationship("Comment", back_populates="owner")
 
-    created = Column(DateTime, default=lambda: datetime.datetime.utcnow())
+    created = Column(
+        DateTime, default=lambda: datetime.datetime.utcnow(), index=True, nullable=False)
     last_modified = Column(DateTime, default=lambda: datetime.datetime.utcnow(
-    ), onupdate=datetime.datetime.utcnow())
+    ), onupdate=datetime.datetime.utcnow(), index=True, nullable=False)
+
+    def serializer(self):
+        return {
+            "id": self.id,
+            "username": self.username,
+            "email": self.email,
+            "active": self.active,
+            "is_admin": self.is_admin,
+            "created": self.created,
+            "last_modified": self.last_modified,
+        }
+
+    def __repr__(self):
+        return "<User(username='{}')>".format(self.username)
