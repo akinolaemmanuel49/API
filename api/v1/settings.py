be67a66071228a0154c2844b1f2cc0acca0f675b
Abiodun Akinola <akinolaemmanuel49@gmail.com>
2022-04-11 16:02:10 +0100

    minor changes

--- api/v1/settings.py
+++ api/v1/settings.py
@@ -2,9 +2,9 @@ import os
 
 from dotenv import load_dotenv
 
-# BASE_DIR = os.path.join(os.path.dirname(
-#     os.path.dirname(os.path.dirname(os.path.abspath(__file__)))), '.env')
-# load_dotenv(BASE_DIR)
+BASE_DIR = os.path.join(os.path.dirname(
+    os.path.dirname(os.path.dirname(os.path.abspath(__file__)))), '.env')
+load_dotenv(BASE_DIR)
 
 
 class Settings:
