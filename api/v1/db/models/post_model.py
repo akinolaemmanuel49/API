03d7153a81028dfb891fcaa37a82da985d8fbf06
Abiodun Akinola <akinolaemmanuel49@gmail.com>
2022-04-11 14:57:30 +0100

    added serializers to post and comment models

--- api/v1/db/models/post_model.py
+++ api/v1/db/models/post_model.py
@@ -2,7 +2,6 @@ import datetime
 
 from sqlalchemy import Column, ForeignKey, Integer, String, DateTime
 from sqlalchemy.orm import relationship
-from sqlalchemy.dialects import postgresql
 
 from ..config import Base
 
@@ -21,3 +20,16 @@ class Post(Base):
     created = Column(DateTime, default=lambda: datetime.datetime.utcnow())
     last_modified = Column(DateTime, default=lambda: datetime.datetime.utcnow(
     ), onupdate=datetime.datetime.utcnow())
+
+    def serializer(self):
+        return {
+            "id": self.id,
+            "title": self.title,
+            "body": self.body,
+            "owner_id": self.owner_id,
+            "created": self.created,
+            "last_modified": self.last_modified
+        }
+
+    def __repr__(self) -> str:
+        return "<Post(title='{}', owner_id='{}')>".format(self.title, self.owner_id)
