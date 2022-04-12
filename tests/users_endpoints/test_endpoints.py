a72900296e3faa99bac4a2f90378010f6434fd65
Abiodun Akinola <akinolaemmanuel49@gmail.com>
2022-04-12 16:41:16 +0100

    modified tests directory structure

---
+++ tests/users_endpoints/test_endpoints.py
@@ -0,0 +1,71 @@
+from tests.test_api import client
+
+
+def test_create_user():
+    response = client.post(
+        "/users/", json={"username": "testuser0", "email": "testuser0@mail.com", "password": "password", "confirm_password": "password"})
+    assert response.status_code == 201
+
+
+def test_create_user_duplicate_email():
+    response = client.post(
+        "/users/", json={"username": "testuser1", "email": "testuser0@mail.com", "password": "password", "confirm_password": "password"})
+    assert response.status_code == 400
+
+
+def test_create_user_duplicate_username():
+    response = client.post(
+        "/users/", json={"username": "testuser0", "email": "testuser1@mail.com", "password": "password", "confirm_password": "password"})
+    assert response.status_code == 400
+
+
+def test_create_user_missing_username_field():
+    response = client.post(
+        "/users/", json={"email": "testuser1mail.com", "password": "password", "confirm_password": "password"})
+    assert response.status_code == 422
+
+
+def test_create_user_missing_email_field():
+    response = client.post(
+        "/users/", json={"username": "testuser1", "password": "password", "confirm_password": "password"})
+    assert response.status_code == 422
+
+
+def test_create_user_missing_password_field():
+    response = client.post(
+        "/users/", json={"username": "testuser1", "email": "testuser1mail.com"})
+    assert response.status_code == 422
+
+
+def test_create_user_validate_email():
+    response = client.post(
+        "/users/", json={"username": "testuser1", "email": "testuser1mail.com", "password": "password", "confirm_password": "password"})
+    assert response.status_code == 422
+
+
+def test_get_users():
+    response = client.get("/users/")
+    assert response.status_code == 200
+
+
+def test_login():
+    response = client.post(
+        "/users/login", json={"username": "testuser0", "password": "password"})
+    assert response.status_code == 200
+
+
+def test_login_missing_username():
+    response = client.post(
+        "/users/login", json={"password": "password"})
+    assert response.status_code == 422
+
+
+def test_login_missing_password():
+    response = client.post(
+        "/users/login", json={"username": "testuser0"})
+    assert response.status_code == 422
+
+
+def test_delete_user():
+    response = client.delete("/users/?username=testuser0")
+    assert response.status_code == 200
